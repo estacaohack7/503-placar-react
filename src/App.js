05e8712214a1b0bec59d7b3a12c2c34ea10438fb
Renan Rodrigo <renanrb@ime.usp.br>
2018-08-02 13:47:03 -0300

    Trocar os nomes dos times em tempo real

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      time1: '',
+      time2: '',
       gols1: 0,
       gols2: 0
     }
@@ -23,19 +25,19 @@ class App extends Component {
       gols2: 0
     });
   }
-  
+
   render() {
     return (
       <div>
         <div className="controles">
-          <input type="text" placeholder="Digite o nome do time da casa" />
-          <input type="text" placeholder="Digite o nome do time visitante" />
+          <input name="1" onChange={evento => this.setState({time1: evento.target.value})} type="text" placeholder="Digite o nome do time da casa" value={this.state.time1} />
+          <input name="2" onChange={evento => this.setState({time2: evento.target.value})} type="text" placeholder="Digite o nome do time visitante" value={this.state.time2}/>
           <button onClick={this.atualizar}>Zerar placar</button>
         </div>
         <div className="placar">
-          <PlacarTime numero="1" gols={this.state.gols1} nomeTime="Corinthians" funcaoBotao={this.incrementar} />
+          <PlacarTime numero="1" gols={this.state.gols1} nomeTime={this.state.time1} funcaoBotao={this.incrementar} />
           X
-          <PlacarTime numero="2" gols={this.state.gols2}nomeTime="Chapecoense" funcaoBotao={this.incrementar} visitante="true"/>
+          <PlacarTime numero="2" gols={this.state.gols2}nomeTime={this.state.time2} funcaoBotao={this.incrementar} visitante="true"/>
         </div>
       </div>
     );
